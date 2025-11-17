c741a4d5249a64803680e7b95b03b550026183f0
Admin <mattalnet@gmail.com>
2025-11-17 10:43:46 +0000

    Fix: Use getters for walletProvider and account to avoid SSR issues

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,29 +1,37 @@
 import "@styles/globals.css";
 import { UserConnectWalletContext } from "Helpers/MultiWallets";
-import { createTheme,ThemeProvider } from '@mui/material/styles'
-import connectMultiWallet from "Helpers/MultiWallets";
-import { disconnectWallet } from "Helpers/MultiWallets";
-import { walletProvider } from "Helpers/MultiWallets";
-import { account } from "Helpers/MultiWallets";
-const theme =createTheme({
-  palette:{
-    primary:{
-      main:"#F9A829"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import connectMultiWallet, { disconnectWallet } from "Helpers/MultiWallets";
+import * as MultiWallets from "Helpers/MultiWallets";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F9A829"
     },
-    mode:"light"
+    mode: "light"
   },
 });
 
 function MyApp({ Component, pageProps }) {
+  const contextValue = {
+    connectMultiWallet,
+    disconnectWallet,
+    get walletProvider() {
+      return MultiWallets.walletProvider;
+    },
+    get account() {
+      return MultiWallets.account;
+    }
+  };
+
   return (
-    <UserConnectWalletContext.Provider value={{connectMultiWallet,disconnectWallet,walletProvider,account}}>
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UserConnectWalletContext.Provider value={contextValue}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </UserConnectWalletContext.Provider>
-
-
-  )
+  );
 }
 
 export default MyApp;
